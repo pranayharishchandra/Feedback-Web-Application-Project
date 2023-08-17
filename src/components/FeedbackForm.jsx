89876b42c76941187af8ba28b8e85f5aed98975a
PRANAY <pranayharishchandra@gmail.com>
2023-08-17 21:46:09 +0530

    Text Change handler created, bug is it works 1 char/operation/state late

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,30 +4,56 @@ import Button from "./shared/Button";
 
 
 function FeedbackForm() {
-    const [text, setText] = useState("");
+    const [text, setText] = useState("")
+    const [btnDisabled, setBtnDisabled] = useState(true) // it will be disabled until we write 10 characters
+    const [message, setMessage] = useState("")
 
+
+
+    
+    
+    
     function textChangeHandler (e) {
-        setText(e.target.value);
+        
+        if (text.length === 0) {
+            setMessage(null);
+            // setMessage("");
+            setBtnDisabled(true);
+        } 
+        else if (text.trim().length < 10) {
+            setMessage("Type atleast 10 characters");
+        } 
+        else {
+            setMessage(null);
+    
+        }
         console.log(text);
+
+        setText(e.target.value);
     }
 
 
 
   return (
     <Card>
-      HOW WOULD YOU RATE THIS SERVICE
-      <div className="input-group">
-            <input type="text" 
-                   placeholder="write your review here"
-                   value={text}
-                   onChange={textChangeHandler} />
+        HOW WOULD YOU RATE THIS SERVICE
+        <div className="input-group">
+                <input type="text" 
+                    placeholder="write your review here"
+                    value={text}
+                    onChange={textChangeHandler} />
 
-            <Button type="submit"
-                    version='secondary'
-                    >send</Button>
+                <Button type="submit"
+                        // version='secondary'
+                        isDisabled={btnDisabled}
 
-      </div>
-            <p>Current input value: {text}</p>
+                        >
+                            Send
+                        </Button>
+
+        </div>
+        {message}
+
 
     </Card>
   )
